20e476a958441e683ea53ec4a154a965caac1a9c
Nauxie <abhinav.tumu1@gmail.com>
2019-01-27 21:50:10 -0800

    app edits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function MyInfo() {
+  return (
+  <div>
+  <h1> Abhinav </h1>
+  <p> I am a high school student. I like to study physics, chemistry, programming, and geography. I am involved in Science Olympiad, Spanish Club, and several local coding competitions.</p>
+  <h4> I would like to visit: </h4>
+  <ul>
+    <li> Tuscany, Italy </li>
+    <li> Colonge, Germany </li>
+    <li> Fort Lauderdale, Florida </li>
+  </ul>  
+  </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <MyInfo/>
       </div>
     );
   }
