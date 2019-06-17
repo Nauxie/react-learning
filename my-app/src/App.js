9bbf95529e709a69dad677e8406bf1479b5c005b
Nauxie <abhinav.tumu1@gmail.com>
2019-06-17 00:13:26 -0700

    todo phase 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import todos from "./todosData.js"
+import todosData from "./todosData.js"
 import TodoItem from "./components/TodoItem"
 import './App.css';
 
 
 
 class App extends Component {
+constructor() {
+  super()
+  this.state = {
+    todos: todosData
+  }
+}
+
   render() {
-    //console.log(products)
-    const todosList = todos.map(todo => {
+    const todosItems = this.state.todos.map(todo => {
       return (<TodoItem list = {todo} key= {todo.id}/>)
     })
-      
-    
     return (
       <div className="todo-list">
-        {todosList}
+        {todosItems}
       </div>
     );
   }
