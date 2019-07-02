e1720261fc919b1af7157042abdb462a8899eefd
Nauxie <abhinav.tumu1@gmail.com>
2019-07-01 22:18:03 -0700

    conditional rendering practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,24 +3,36 @@ import React from "react"
 class App extends React.Component {
     constructor() {
         super()
+        this.handleClick = this.handleClick.bind(this)
         this.state = {
             isLoggedIn: false
         }
         
     }
-    
+    handleClick() {
+        let invert = this.state.isLoggedIn
+        this.setState({
+            isLoggedIn: !invert
+        })
+
+    }
     render() {
         let status = ""
+        let altstatus
         if (this.state.isLoggedIn) {
             status = "in"
+            altstatus = "out"
         }
         else {
             status = "out"
+            altstatus = "in"
         }
         
         return (
             <div>
+                <button onClick={this.handleClick}>Log {altstatus}</button>
                 <h1>You are currently logged {status}</h1>
+                
             </div>
             
         )
